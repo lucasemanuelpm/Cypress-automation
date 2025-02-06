d92d139397e5dabc8f1d756c386e2f2e1ce0b958
Lucas Emanuel Perim Martins <142339368+lucasemanuelpm@users.noreply.github.com>
2025-02-06 20:44:29 -0300

    oo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,7 +8,7 @@ describe('template spec', () => {
   })
   it.only('login inválido', () => {
     cy.visit('https://www.saucedemo.com/v1/')
-    cy.get('[data-test="username"]').type("anonimo_user")
+    cy.get('[data-test="username"]').type("lucas_user")
     cy.get('[data-test="password"]').type("secret_sauce")
     cy.get('#login-button').click()
     cy.get('[data-test="error"]').contains("Epic sadface: Username and password do not match any user in this service")
